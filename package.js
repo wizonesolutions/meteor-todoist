f80e99d84bc1b54ec964d701b4bb1639b685f3cf
Kevin Kaland <kevin@wizonesolutions.com>
2014-09-11 14:44:10 +0200

    Bump version.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'wizonesolutions:todoist',
   summary: "Wrapper for the Todoist API.",
-  version: "1.0.0",
+  version: "1.0.1",
   git: "https://github.com/wizonesolutions/meteor-todoist"
 });
 
